6dc0dd39b6db77381a95d5d7163b0e14dc1a503e
Halerium <hal@erium.ai>
2023-02-13 15:27:52 +0000

    updated templates repo

---
+++ use_case_templates/data_preparation/hooks/post_gen_project.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+import halerium_utilities as hu
+from halerium_utilities import notebook
+
+hu.file.card_ids.assign_new_card_ids_to_tree('./')
+
+print("Hal_Magic_Template_done.")
